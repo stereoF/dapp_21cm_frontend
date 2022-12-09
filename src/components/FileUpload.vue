96812204c6458f355b61f40a64695e4867b9910c
stereoF <firmamentsess@gmail.com>
2022-12-10 01:14:05 +0800

    add filelist manager

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -5,6 +5,17 @@
     @change="readFile" 
     multiple="multiple"
   />
+
+  <el-button type="primary">
+      Upload<el-icon class="el-icon--right"><Upload /></el-icon>
+  </el-button>
+
+  <h2>Files to Upload</h2>
+  <ul>
+    <li v-for="file in files">
+      {{ file.name }} ({{ file.size | kb }} kb) <button @click="removeFile(file)" title="Remove">X</button>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -13,12 +24,20 @@ import { create } from 'ipfs-http-client'
 export default {
   data() {
     return {
+      files:[]
     }
   },
   methods:{
     async readFile() {
-      let files = Array.from(this.$refs.file.files)
-      let fileObjectsArray = files.map((file) => {
+      this.files = Array.from(await this.$refs.file.files)
+    },
+    removeFile(file){
+      this.files = this.files.filter(f => {
+        return f != file;
+      });      
+    },
+    async upload() {
+      let fileObjectsArray = this.files.map((file) => {
         return {
           path: file.name,
           content: file
@@ -27,12 +46,12 @@ export default {
       // const ipfs = await IPFS.create()
       const ipfs = await new create('http://127.0.0.1:5001')
       const result = []
-      for await (const resultPart of ipfs.addAll(files, { wrapWithDirectory: true })) {
+      for await (const resultPart of ipfs.addAll(fileObjectsArray, { wrapWithDirectory: true })) {
         result.push(resultPart)
       }
       // result = await Promise.all(ipfs.addAll(fileObjectsArray), { wrapWithDirectory: true })
       console.log(result)
-    }
+    },
   }
 }
 </script>
