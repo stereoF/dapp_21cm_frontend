cae0341154b9c7016c854cbfd753f6a14e5c7322
stereoF <firmamentsess@gmail.com>
2023-04-04 21:33:11 +0800

    modify ManagerFields.vue

---
+++ src/components/ManagerFields.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="form-group">
+        <label>{{$props.fieldName}}s:</label>
+        <div v-for="(field, index) in fields" :key="index" class="row align-items-center">
+            <div class="col-10 pr-1">
+                <input type="text" v-model="field.field" :placeholder="$props.fieldName"  class="form-control" />
+            </div>
+            <div class="col-1 item-buttons">
+                <div>
+                    <button type="button" @click="addField(index)" class="btn btn-success btn-sm">+</button>
+                </div>
+                <div>
+                    <button type="button" @click="removeField(index)" class="btn btn-danger btn-sm">-</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent, reactive } from 'vue';
+
+    export default defineComponent({
+        name: 'ManagerFields',
+        props: {
+            fieldName: {
+                type: String,
+                required: true
+            },
+            fields: {
+                type: Array<{field: String}>,
+                required: false,
+                // default: () => [{ field: '' }],
+                // validator: (arr: {field: string}[]) => {
+                //     return arr.every(item => typeof item.field === 'string');
+                // }
+            }
+        },
+        setup(props) {
+            const fields = reactive(props.fields || [{ field: '' }]);
+            // const fields = reactive([{ field: '' }]);
+            if (fields.length === 0) {
+                fields.push({ field: '' })
+            };
+
+            function addField(index: number) {
+                fields.splice(index + 1, 0, { field: '' })
+            }
+
+            function removeField(index: number) {
+                fields.splice(index, 1)
+            }
+
+            return {
+                fields,
+                addField,
+                removeField
+            }
+        }
+    })
+    
+
+</script>
+
+<style>
+.item-buttons {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.item-buttons button {
+  margin-left: 8px;
+}
+</style>
--- src/pages/deSci/OwnerPages/AssignEditors.vue
+++ src/pages/deSci/OwnerPages/AssignEditors.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="container">
       <!-- Title -->
-      <h1>{{ title }}</h1>
+      <h1>Manager the editors of journal: {{ title }}</h1>
       <form @submit.prevent="contractCall">
-        <ManagerFields field-name="Editor"/>
+        <!-- <ManagerFields field-name="Editor" :fields="editors.map(editor => ({field: editor}))"/> -->
+        <ManagerFields field-name="Editor" :fields="editors.map((editor: any) => ({field: editor}))"/>
         <div class="row mt-2">
           <div class="col-12 col-sm-8">
             <button id="contractCall" type="submit" class="btn btn-primary">Submit</button>
@@ -18,10 +19,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, reactive } from 'vue';
 import { ethers } from "ethers";
 import DeSciPrint from "@/contracts/desci/DeSciPrint.json";
-import ManagerFields from '../ManagerFields.vue';
+import ManagerFields from '@/components/ManagerFields.vue';
 
 export default defineComponent({
     name: "AssignEditors",
@@ -35,8 +36,7 @@ export default defineComponent({
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const deSciPrint = new ethers.Contract(props.address as string, DeSciPrint.abi, provider);
         const title = ref(await deSciPrint.name());
-        const editors = await deSciPrint.editors();
-        console.log(editors);
+        const editors = reactive(await deSciPrint.editors());
         let submitFailed = ref(false);
         let submitSucceed = ref(false);
         const contractCall = async () => {
@@ -45,7 +45,8 @@ export default defineComponent({
             title,
             contractCall,
             submitFailed,
-            submitSucceed
+            submitSucceed,
+            editors
         };
     },
     components: { ManagerFields }
